f3f84bfa36b92a1c5fdc533d672f94ab7aa2a453
Owen <93125379+owen-kainos@users.noreply.github.com>
2025-01-28 22:24:10 +0000

    Updated authentication emails

--- Web/Services/EmailSender.cs
+++ Web/Services/EmailSender.cs
@@ -10,16 +10,43 @@ public class EmailSender(IMailService mailService) : IEmailSender<ApplicationUse
     
     public async Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink)
     {
-        await MailService.SendEmailAsync(email, "Email Confirmation", HttpUtility.HtmlDecode(confirmationLink));
+        var link = HttpUtility.HtmlDecode(confirmationLink);
+
+        var message = $@"
+            <h1>Email Confirmation</h1>
+            <p>Dear {user.UserName},</p>
+            <p>Thank you for registering with us. Please confirm your email by clicking on the link below.</p>
+            <a href=""{link}"">Confirm Email</a>
+            <p>Thank you!</p>";
+
+        await MailService.SendEmailAsync(email, "Email Confirmation", message);
     }
 
     public async Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink)
     {
-        await MailService.SendEmailAsync(email, "Password Reset Link", HttpUtility.HtmlDecode(resetLink));
+        var link = HttpUtility.HtmlDecode(resetLink);
+        
+        var message = $@"
+            <h1>Password Reset</h1>
+            <p>Dear {user.UserName},</p>
+            <p>We received a request to reset your password. Please click on the link below to reset your password.</p>
+            <a href=""{link}"">Reset Password</a>
+            <p>Thank you!</p>";
+        
+        await MailService.SendEmailAsync(email, "Password Reset Link", message);
     }
 
     public async Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode)
     {
-        await MailService.SendEmailAsync(email, "Password Reset Code", HttpUtility.HtmlDecode(resetCode));
+        var code = HttpUtility.HtmlDecode(resetCode);
+        
+        var message = $@"
+            <h1>Password Reset</h1>
+            <p>Dear {user.UserName},</p>
+            <p>We received a request to reset your password. Please use the following code to reset your password.</p>
+            <p><strong>{code}</strong></p>
+            <p>Thank you!</p>";
+        
+        await MailService.SendEmailAsync(email, "Password Reset Code", message);
     }
 }
